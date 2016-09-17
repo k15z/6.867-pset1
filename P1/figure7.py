43346790a1503a81824147183e70c46c41a1b31c
Felipe_Hofmann <fealho@mit.edu>
2016-09-17 17:45:05 -0400

    figure 8
    Gaussian step size per echo

---
+++ P1/figure7.py
@@ -0,0 +1,55 @@
+import math
+import random
+import numpy as np
+import loadParametersP1
+from matplotlib import pyplot as plt
+
+def grad_desc(func, d_func, stepSize, init=np.zeros(2)):
+    x = init
+    values = []
+    for epoch in range(0,5000):
+        x -= stepSize * d_func(x)
+        values += [func(x)]
+    print(x)
+    return values
+
+gaussMean,gaussCov,quadBowlA,quadBowlb = loadParametersP1.getData()
+def gfunc(x):
+    diff = (x - gaussMean)
+    icov = np.linalg.inv(gaussCov)
+    norm = - 1.0 / math.sqrt((2 * 3.1415)**len(gaussMean) * np.linalg.norm(gaussCov))
+    return norm * np.exp(- 0.5 * diff.transpose().dot(icov.dot(diff)))
+def d_gfunc(x):
+    diff = (x - gaussMean)
+    icov = np.linalg.inv(gaussCov)
+    return -gfunc(x) * icov.dot(diff)
+def qfunc(x):
+    return 0.5 * x.transpose().dot(quadBowlA.dot(x)) - x.transpose().dot(quadBowlb)
+def d_qfunc(x):
+    return quadBowlA.dot(x) - quadBowlb
+def ad_qfunc(x):
+    result = [
+        (qfunc(x+np.array([0.1,0])) - qfunc(x-np.array([0.1,0]))) / 0.2,
+        (qfunc(x+np.array([0,0.1])) - qfunc(x-np.array([0,0.1]))) / 0.2
+    ]
+    return np.array(result)
+
+pairs = [
+    (10000, 'c-'),
+    (1000, 'r-'),
+    (100, 'g-'),
+    (10, 'b-'),
+    (1, 'm-')
+]
+
+handles = []
+for pair in pairs:
+    stepSize, color = pair
+    val = str(stepSize)
+    y_values = grad_desc(gfunc, d_gfunc, stepSize, init=[-10,10])
+    handles += [plt.plot(y_values, color, label=val)[0]]
+plt.legend(handles=handles)
+plt.title('Gradient Descent: Gaussian')
+plt.xlabel('epoch')
+plt.ylabel('f(x)')
+plt.show()
